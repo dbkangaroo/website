0b2fff9b57111b5f435617c85ad916c3de671fc0
taozuhong <taozuhong@gmail.com>
2022-07-29 13:11:24 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -23,8 +23,8 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:dbkangaroo/dbkangaroo.github.io.git master
-# git push -f https://github.com/dbkangaroo/dbkangaroo.github.io.git master
+# git push -f git@github.com:dbkangaroo/dbkangaroo.github.io.git master
+git push -f https://github.com/dbkangaroo/dbkangaroo.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
